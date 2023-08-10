21a1646aead0776e4b99825c6bfcbb9c5a0e73fd
Hashim2009 <94174587+Hashim2009@users.noreply.github.com>
2023-08-10 20:01:51 +0800

    Add files via upload

---
+++ tvandac.js
@@ -0,0 +1,31 @@
+function preload() {
+    img = loadImage('tvandac.jpeg');
+}
+
+function setup() {
+    canvas = createCanvas(640, 420);
+    canvas.center();
+}
+
+function draw() {
+    image(img, 0, 0, 640, 420);
+    fill(255, 0, 0);
+    text('Bed', 150, 275);
+    noFill();
+    stroke(255, 0, 0);
+    rect(15, 240, 300, 350);
+    fill(0, 255, 0);
+    text('Fan', 280, 50);
+    noFill();
+    stroke(0, 255, 0);
+    rect(159, 5, 150, 100);
+    fill(0, 0, 255);
+    text('Monitor', 350, 250);
+    noFill();
+    stroke(0, 0, 255);
+    rect(350, 230, 100, 50);
+}
+
+function home() {
+    window.location = "index.html";
+}
